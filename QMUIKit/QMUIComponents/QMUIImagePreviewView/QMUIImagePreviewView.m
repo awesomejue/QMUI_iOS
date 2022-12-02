52e5f5bacce56cc2acc86031cb9bfb3e973597fd
ligang06 <ligang06@kuaishou.com>
2022-12-02 17:36:00 +0800

    修复图片展示

--- QMUIKit/QMUIComponents/QMUIImagePreviewView/QMUIImagePreviewView.m
+++ QMUIKit/QMUIComponents/QMUIImagePreviewView/QMUIImagePreviewView.m
@@ -105,14 +105,17 @@ static NSString * const kImageOrUnknownCellIdentifier = @"imageorunknown";
     
     BOOL isCollectionViewSizeChanged = !CGSizeEqualToSize(self.collectionView.bounds.size, self.bounds.size);
     
-    if (isCollectionViewSizeChanged && self.collectionView.visibleCells.count > 0) {
+    if (isCollectionViewSizeChanged) {
         
         self.isChangingCollectionViewBounds = YES;
         
         // 必须先 invalidateLayout，再更新 collectionView.frame，否则横竖屏旋转前后的图片不一致（因为 scrollViewDidScroll: 时 contentSize、contentOffset 那些是错的）
         [self.collectionViewLayout invalidateLayout];
         self.collectionView.frame = self.bounds;
-        [self.collectionView scrollToItemAtIndexPath:[NSIndexPath indexPathForItem:self.currentImageIndex inSection:0] atScrollPosition:UICollectionViewScrollPositionCenteredHorizontally animated:NO];
+        
+        if (self.collectionView.visibleCells.count > 0) {
+            [self.collectionView scrollToItemAtIndexPath:[NSIndexPath indexPathForItem:self.currentImageIndex inSection:0] atScrollPosition:UICollectionViewScrollPositionCenteredHorizontally animated:NO];
+        }
         
         self.isChangingCollectionViewBounds = NO;
     }
